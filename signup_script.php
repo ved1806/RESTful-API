50569569678b96bb0d0f43ce0c1e1d3a21f04971
Ved1806 <vdntkatti@gmail.com>
2022-01-18 19:43:58 +0530

    first commit

---
+++ signup_script.php
@@ -0,0 +1,33 @@
+<?php
+require 'includes/common.php';
+$con = mysqli_connect("localhost","root","","store")or die(mysqli_error($con));
+$name=$_POST['name'];
+$email=$_POST['email'];
+$password =$_POST['password'];
+$password = ($password);
+$uppercase = preg_match('@[A-Z]@', $password);
+$lowercase = preg_match('@[a-z]@', $password);
+$number    = preg_match('@[0-9]@', $password);
+$specialChars = preg_match('@[^\w]@', $password);
+
+$contact=$_POST['contact'];
+$city=$_POST['city'];
+$address=$_POST['address'];
+$query = "select * from users where email='$email'";
+$res = mysqli_query($con,$query)  or die("failed ".mysqli_error($con));
+$array = mysqli_fetch_array($res);
+if($email == $array['email'])
+{
+	echo "<h3>User already exists! Try another username.</h3>";
+}
+else
+{
+	$insert = "insert into users (name, email, passwd, contact, city, address) values('$name', '$email', '$password', '$contact', '$city', '$address')";
+	$r = mysqli_query($con,$insert)  or die("failed ".mysqli_error($con));
+	session_start();
+	$id =  mysqli_insert_id($con);
+	$_SESSION['email'] = $email;
+	$_SESSION['id'] = $id;
+	header("Location: products.php");
+}
+?>
